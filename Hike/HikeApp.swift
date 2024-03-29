ae2f94ac70d8e3499e601379a45e09a524317e63
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-03-29 09:34:08 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by vinay Kumar ranjan on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
